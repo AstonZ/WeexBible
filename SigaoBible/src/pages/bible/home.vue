35267c3b5cb4cd475e374ba91d0066563a620b12
Aston K Mac <zhangliang203@pingan.com.cn>
2018-02-19 13:33:55 +0800

    - 修改样式失败

--- SigaoBible/src/pages/bible/home.vue
+++ SigaoBible/src/pages/bible/home.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="wrapper">
-        <text class="text-holder">Bible Home</text>
+        <text class="text-holder">Bible Home Ha</text>
         <div class="img-wrapper">
             <image class="img-cover" resize="stretch" 
-            :src="get_img_path('home-placeholder.png')>
-            </image>
+            :src="get_img_path('logo.png')"/>
         </div>
     </div>
-
 </template>
+
+
 <script>
     export default {
         data () {
@@ -29,16 +29,16 @@
         bottom: 0;
         left: 0;
         right: 0;
-        background-color: #333
+        background-color:black
     }
 
     .img-wrapper {
         position: fixed;
-        top:0;
+        top:50;
         left:0;
-        right:0;
+        width: 300;
         height: 200;
-        background-color: #fff;
+        background-color: #fff
     }
 
     .img-cover {
@@ -46,13 +46,14 @@
         height: 200;
     }
     
+
     .text-holder {
         height: 66;
         font-size: 26;
         padding-top: 16;
         flex: 1;
         text-align: center;
-        color: blueviolet;
+        color: #41B883;
     }
 </style>
 
